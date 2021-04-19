0c4a0b37636fc8ffb6087001066d434debeb64f3
Richard B <r.benitez723@gmail.com>
2021-04-19 15:53:23 -0400

    new

--- app/src/main/java/com/example/pizzaplanetapp/MainActivity.java
+++ app/src/main/java/com/example/pizzaplanetapp/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.pizzaplanetapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView videov;
+    MediaController mediaC;
+    public Button pizzaMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playVideo();
+        pizzaMenu = (Button) findViewById(R.id.pizzaMenuButton);
+        pizzaMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PizzaMenu.class));
+            }
+
+        });
+/*        pizzaMenu = (Button) findViewById(R.id.pizzaMenuButton);
+        pizzaMenu.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(this, PizzaMenu.class);
+                startActivity(intent);
+
+            }
+
+        });*/
+
     }
+
+    public void playVideo(){
+        videov = (VideoView) findViewById(R.id.videoView);
+        mediaC = new MediaController(this);
+        videov.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.toystorypizzaplanet);
+        mediaC.setAnchorView(videov);
+        videov.setMediaController(mediaC);
+        videov.start();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        videov.start();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        videov.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        videov.suspend();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        videov.stopPlayback();
+    }
+
+
+/*    public void openPizzaMenu(View view) {
+        Intent intent = new Intent(this, PizzaMenu.class);
+        startActivity(intent);
+
+
+    }*/
 }
---
+++ app/src/main/java/com/example/pizzaplanetapp/PizzaMenu.java
@@ -0,0 +1,15 @@
+package com.example.pizzaplanetapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PizzaMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pizza_menu);
+
+
+    }
+}
