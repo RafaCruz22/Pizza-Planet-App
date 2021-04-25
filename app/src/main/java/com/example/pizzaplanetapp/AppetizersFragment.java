4fbae37bdc2b17925c4f9886c48f06d43e2c0d68
213412341345234523452352345234342@users.noreply.github.com <martinmartinez9@yahoo.com>
2021-04-25 00:08:32 -0400

    Added a style so all font is consistent, change the style in the styles to change it for all font. Cleaned up code, added to appetizers and beverages

--- app/src/main/java/com/example/pizzaplanetapp/AppetizersFragment.java
+++ app/src/main/java/com/example/pizzaplanetapp/AppetizersFragment.java
@@ -1,6 +1,21 @@
 package com.example.pizzaplanetapp;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class AppetizersFragment extends Fragment {
+
+    public static final String TAG = "AppetizerFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.appetizers, container, false);
+    }
 }
--- app/src/main/java/com/example/pizzaplanetapp/BeveragesFragment.java
+++ app/src/main/java/com/example/pizzaplanetapp/BeveragesFragment.java
@@ -1,6 +1,32 @@
 package com.example.pizzaplanetapp;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BeveragesFragment extends Fragment {
+
+    public static final String TAG = "BevFragment";
+    private TextView tv_water, tv_sprite, tv_coke, tv_pepsi, tv_icedTea;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return inflater.inflate(R.layout.beverages, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+        //potentially add long click listeners to add to cart, conferring with team
+        //tv_water = getActivity().findViewById(R.id.bev_water);
+    }
 }
